11217cb380ea73f5b0d48aa9ceb265f1442c91e2
Nathan D <truenathand@gmail.com>
2022-04-15 06:47:52 -0500

    Advanced project progress in index.js, taskManager.js

--- js/index.js
+++ js/index.js
@@ -1,58 +1,111 @@
+const taskManager = new TaskManager(0);
+const newTaskManager = new TaskManager(0);
+
+// Select the New Task Form
 const newTaskForm = document.querySelector('#newTaskForm');
-// made sure form was connected to task manager
-newTaskForm.addEventListener('submit', (Event) => {
-    Event.preventDefault();
+
+// Adds event listener
+newTaskForm.addEventListener('submit', (event) => {
+    // Prevents default action
+    event.preventDefault();
+    let messages = [];
+
+    // Selects the inputs
+    const newTaskNameInput = document.querySelector('#newTaskNameInput');
+    const newTaskDescription = document.querySelector('#newTaskDescription');
+    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
+    const newTaskDueDate = document.querySelector('#newTaskDueDate');
+
+
+    // Gets the input's values
+    const taskName = newTaskNameInput.value;
+    const description = newTaskDescription.value;
+    const assignedTo = newTaskAssignedTo.value;
+    const dueDate = newTaskDueDate.value;
+
+taskManager.addTask(taskName, description, assignedTo, dueDate, status);
+
+newTaskNameInput.value = '';
+newTaskDescription.value = '';
+newTaskAssignedTo.value = '';
+newTaskDueDate.value = '';
+
 });
 
-const task1 = {
-    id: 0,
-    name: 'Meditate',
-    description: 'Meditate 15 mins before the start of every day',
-    assignedTo: 'Myself',
-    dueDate: '05/01/2022',
-    status: 'IN PROGRESS'
-    
-};
+const tasksList = document.querySelector('#tasksList');
 
-const task2 = {
-    id: 1,
-    name: 'All in the name of self love',
-    description: 'Cater to myself for an hour',
-    assignedTo: 'Myself',
-    dueDate: '06/01/2022',
-    status: 'NOT STARTED YET'
-    
-};
+tasksList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('done-button')) {
+        const parentTask = event.target.parentElement.parentElement;
 
-const task3 = {
-    id: 2,
-    name: 'Acts of love',
-    description: 'Be kind to receive to every human you interact with to focus on being a more well rounded, thoughtful, loving person',
-    assignedTo: 'Myself',
-    dueDate: '07/01/2022',
-    status: 'NOT STARTED YET'
-    
-};
+        const taskId = Number(parentTask.dataset.taskId);
 
-const task4 = {
-    id: 3,
-    name: 'Positive thoughts',
-    description: 'Read a book that focuses on positivity and happiness',
-    assignedTo: 'Myself',
-    dueDate: '08/01/2022',
-    status: 'NOT STARTED YET'
-    
-};
+        const task = taskManager.getTaskById(taskId);
+
+        task.status = 'DONE';
+
+    }
+});
 
 
-const task5 = {
-    id: 4,
-    name: 'The power of manifestation',
-    description: 'Think it, believe and achieve it by speaking and writing down your goals you want to accomplish in 30 days',
-    assignedTo: 'Myself',
-    dueDate: '09/01/2022',
-    status: 'NOT STARTED YET'
+
+
+// const task1 = {
+//     id: 0,
+//     name: 'Meditate',
+//     description: 'Meditate 15 mins before the start of every day',
+//     assignedTo: 'Myself',
+//     dueDate: '05/01/2022',
+//     status: 'IN PROGRESS'
     
-};
+// };
+
+// const task2 = {
+//     id: 1,
+//     name: 'All in the name of self love',
+//     description: 'Cater to myself for an hour',
+//     assignedTo: 'Myself',
+//     dueDate: '06/01/2022',
+//     status: 'NOT STARTED YET'
+    
+// };
+
+// const task3 = {
+//     id: 2,
+//     name: 'Acts of love',
+//     description: 'Be kind to receive to every human you interact with to focus on being a more well rounded, thoughtful, loving person',
+//     assignedTo: 'Myself',
+//     dueDate: '07/01/2022',
+//     status: 'NOT STARTED YET'
+    
+// };
+
+// const task4 = {
+//     id: 3,
+//     name: 'Positive thoughts',
+//     description: 'Read a book that focuses on positivity and happiness',
+//     assignedTo: 'Myself',
+//     dueDate: '08/01/2022',
+//     status: 'NOT STARTED YET'
+    
+// };
+
+
+// const task5 = {
+//     id: 4,
+//     name: 'The power of manifestation',
+//     description: 'Think it, believe and achieve it by speaking and writing down your goals you want to accomplish in 30 days',
+//     assignedTo: 'Myself',
+//     dueDate: '09/01/2022',
+//     status: 'NOT STARTED YET'
+    
+// };
+
+
+
+newTaskManager.addTask('map', 'pasta', 'coffee', 'hour');
+
+console.log(newTaskManager);
 
 // added the affirmation functions to complete
+
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,15 +1,40 @@
 // updated and used console log to make sure task manager works
 
+                // *** Where we left off ***
+function createTaskHtml() {
+    'name', 'description', 'assignedTo', 'dueDate', 'status'
+
+}
+
+
 class TaskManager {
-    tasks;
-    constructor(myTasks) {
-        this.tasks = "";
-
-    const currentId = [0, 1, 2, 3, 4];
-    this.currentId = currentId;
-
-    };
+    constructor(currentId = 1) {
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+    addTask(name, description, assignedTo, dueDate, status) {
+        const task = {
+            id: this.currentId++,
+            name: name,
+            description: description,
+            dueDate: dueDate,
+            status: 'TODO'
+        };
+        this.tasks.push(task);
 };
 
 
+getTaskById(taskId) {
+    let foundTask;
+
+    for (let i = 0; i < this.tasks.length; i++) {
+        const task = this.tasks[i];
+
+        if (task.id === taskId) {
+            foundTask = task;
+        }
+    }
+    return foundTask;
+}
+}
 // created an array to connect with the task objects
