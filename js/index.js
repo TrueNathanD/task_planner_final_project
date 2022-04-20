f9c24e0e68419157a8e5efc407e536b4fceeda44
Akenyae Jones <akenyaer@gmail.com>
2022-04-20 15:08:03 -0400

    created function in javascript for task form alerts

--- js/index.js
+++ js/index.js
@@ -1,51 +1,4 @@
-// const taskManager = new TaskManager(0);
-// const newTaskManager = new TaskManager(0);
 
-// // Select the New Task Form
-// const newTaskForm = document.querySelector('#newTaskForm');
-
-// // Adds event listener
-// newTaskForm.addEventListener('submit', (event) => {
-//     // Prevents default action
-//     event.preventDefault();
-//     let messages = [];
-
-//     // Selects the inputs
-//     const newTaskNameInput = document.querySelector('#newTaskNameInput');
-//     const newTaskDescription = document.querySelector('#newTaskDescription');
-//     const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-//     const newTaskDueDate = document.querySelector('#newTaskDueDate');
-
-
-//     // Gets the input's values
-//     const taskName = newTaskNameInput.value;
-//     const description = newTaskDescription.value;
-//     const assignedTo = newTaskAssignedTo.value;
-//     const dueDate = newTaskDueDate.value;
-
-// taskManager.addTask(taskName, description, assignedTo, dueDate, status);
-
-// newTaskNameInput.value = '';
-// newTaskDescription.value = '';
-// newTaskAssignedTo.value = '';
-// newTaskDueDate.value = '';
-
-// });
-
-// const tasksList = document.querySelector('#tasksList');
-
-// tasksList.addEventListener('click', (event) => {
-//     if (event.target.classList.contains('done-button')) {
-//         const parentTask = event.target.parentElement.parentElement;
-
-//         const taskId = Number(parentTask.dataset.taskId);
-
-//         const task = taskManager.getTaskById(taskId);
-
-//         task.status = 'DONE';
-
-//     }
-// });
 
 
 
@@ -103,14 +56,6 @@
 
 
 
-// newTaskManager.addTask('map', 'pasta', 'coffee', 'hour');
-
-// console.log(newTaskManager);
-
-// // added the affirmation functions to complete
-
-
-// CAUTION: TEST CODE BEGINS BELOW!
 
 const taskManager = new TaskManager(0);
 
