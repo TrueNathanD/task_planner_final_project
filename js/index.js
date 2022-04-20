cf81183afef839c0bb1339592167018741a4520c
Nathan D Strzelczyk <truenathand@gmail.com>
2022-04-20 13:52:38 -0500

    Added image to image viewer

--- js/index.js
+++ js/index.js
@@ -183,3 +183,24 @@ tasksList.addEventListener('click', (event) => {
         taskManager.render();
     }
 });
+
+
+// Header Image JavaScript
+let modal = document.getElementById("myModal");
+
+let img = document.getElementById("myImg");
+let modalImg = document.getElementById("img01");
+let captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
